259a51ea221e51c238490260d865334cdc4905cd
devabdullahalnoman <nomanahnaf@gmail.com>
2025-11-25 09:03:02 +0600

    attempt to fix seller application approve

--- app/Policies/SellerApplicationPolicy.php
+++ app/Policies/SellerApplicationPolicy.php
@@ -19,6 +19,11 @@ class SellerApplicationPolicy
         return $user->isAdmin();
     }
 
+    public function update(User $user): bool
+    {
+        return $user->isAdmin();
+    }
+
     public function delete(User $user): bool
     {
         return $user->isAdmin();
