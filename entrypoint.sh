68986b2c31cab628628c5549fac8e192422e4c5a
devabdullahalnoman <nomanahnaf@gmail.com>
2025-11-25 08:47:23 +0600

    attempt to fix login

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,8 @@
 php artisan migrate --force
 php artisan db:seed --force
 
+php artisan tinker --execute="App\Models\User::where('email','admin@test.com')->update(['password'=>Illuminate\Support\Facades\Hash::make('admin123')]);"
+
 php artisan config:clear && php artisan view:clear && php artisan cache:clear
 
 # Start PHP server
